c05d150a974df2087b4ad6d9955df45ae2b5f967
Kacper Kaźmierkiewicz <38405516+KaZiUpl@users.noreply.github.com>
2020-06-08 00:56:13 +0200

    created index page module and component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DefaultComponent } from './layouts/default/default.component';
+import { IndexComponent } from './pages/index/index.component';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'index',
-    pathMatch: 'full',
-  },
-  {
-    path: 'index',
     component: DefaultComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'index',
+        pathMatch: 'full',
+      },
+      {
+        path: 'index',
+        component: IndexComponent,
+        loadChildren: () =>
+          import('./pages/index/index.module').then((m) => m.IndexModule),
+      },
+    ],
   },
+  { path: './', loadChildren: () => import('./pages/index/index.module').then(m => m.IndexModule) },
 ];
 
 @NgModule({
