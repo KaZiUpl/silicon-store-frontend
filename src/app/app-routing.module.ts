334dda3fa933eed4f023fbc56b33026cc9224060
Kacper Kaźmierkiewicz <38405516+KaZiUpl@users.noreply.github.com>
2020-06-07 20:48:00 +0200

    added menu and footer components
    -added bootstrap to project
    -changed apiUrl
    -changed font to Montserrat

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,21 +2,20 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DefaultComponent } from './layouts/default/default.component';
 
-
 const routes: Routes = [
   {
     path: '',
     redirectTo: 'index',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
   {
     path: 'index',
     component: DefaultComponent,
-  }
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/layouts/default/default.module.ts
+++ src/app/layouts/default/default.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DefaultComponent } from './default.component';
+import { RouterModule } from '@angular/router';
+import { ComponentsModule } from 'src/app/components/components.module';
 
 
 
 @NgModule({
   declarations: [DefaultComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    ComponentsModule
   ]
 })
 export class DefaultModule { }
