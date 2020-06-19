f6889b848a36d3a47d4a8a772b2c183b0fe06271
Kacper Kaźmierkiewicz <38405516+KaZiUpl@users.noreply.github.com>
2020-06-19 13:24:02 +0200

    added category list component
    Category list displays all items from selected category or categories that are descendants of selected
    Additionally:
    -changed links on main category menu component and breadcrumb component
    -fixed URL on categories service

--- src/app/pages/category-list/category-list.module.ts
+++ src/app/pages/category-list/category-list.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { CategoryListRoutingModule } from './category-list-routing.module';
 import { CategoryListComponent } from './category-list.component';
+import { ComponentsModule } from 'src/app/components/components.module';
 
 
 @NgModule({
   declarations: [CategoryListComponent],
   imports: [
     CommonModule,
+    ComponentsModule,
     CategoryListRoutingModule
   ]
 })
